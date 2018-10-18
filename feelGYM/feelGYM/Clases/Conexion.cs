ce674f5a9539ce7b165b79d83758be646fe2a801
Mayco Garelli <maycogarelli@gmail.com>
2018-10-18 14:43:55 -0300

    sincronizar

--- feelGYM/feelGYM/Clases/Conexion.cs
+++ feelGYM/feelGYM/Clases/Conexion.cs
@@ -11,7 +11,7 @@ namespace feelGYM.Clases
     public class Conexion
     {
         public static MySqlConnection obtenerConexion() {
-            MySqlConnection conexion = new MySqlConnection("datasource=192.168.0.113; Uid=mayco; password=1234; Database=feelgym; SslMode=none");
+            MySqlConnection conexion = new MySqlConnection("datasource=192.168.0.16; Uid=mayco; password=1234; Database=feelgym; SslMode=none");
             //MySqlConnection conexion = new MySqlConnection("datasource=192.168.0.16; Uid=mayco; password=1234; Database=feelgym; SslMode=none");
             //MySqlConnection conexion = new MySqlConnection("datasource=localhost; Uid=mayco; password=1234; Database=feelgym; SslMode=none");
             conexion.Open();
